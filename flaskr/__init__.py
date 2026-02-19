c8c02a47a0759c35b7d737d4b52becaaee6965ec
sriram kallidaikurichi <skallida@gmail.com>
2026-02-18 23:47:49 -0600

    bugfix

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,6 +8,10 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
     
+    @app.route("/")
+    def index():
+        return "Flask login App is live!!"
+
     if test_config is None:
         # Load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
